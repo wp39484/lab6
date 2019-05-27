f18ee76c9ccc333fc5abd4b5a1df893050d8b36b
pawel <wp39484@zut.edu.pl>
2019-05-28 00:02:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lab6;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import pl.antons.rsatest.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Method invoke by clicking the Button
+    public void gameList(View v)
+    {
+        Intent intencja = new Intent(getApplicationContext(), GamesList.class);
+        //Sending message what game was choose
+        intencja.putExtra("gra", v.getId());
+        startActivity(intencja);
+    }
+
+
+
+
+
+
+}
